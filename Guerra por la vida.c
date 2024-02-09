172d5db8a03742f83fa32b68a7e3f489a6bea441
JAndresNav <149545903+JAndresNav@users.noreply.github.com>
2024-02-08 20:20:36 -0600

    actualización
    Ya funciona a medias, la funcion juego de la vida ya evalúa aunque solo las células X o eso creo jsjs

--- Guerra por la vida.c
+++ Guerra por la vida.c
@@ -5,9 +5,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-void crearTablero(char *tablero,int col,int row){
+void crearTablero(char *tablero, int col, int row) {
     time_t t;
-    srand((unsigned) time(&t));
+    srand((unsigned)time(&t));
     int temp;
     int contadorX = 0;
     int contadorO = 0;
@@ -16,68 +16,118 @@ void crearTablero(char *tablero,int col,int row){
         for (int j = 0; j < row; ++j) {
             temp = rand() % 3;
 
-            if(temp != 0 && contadorO == 50 && contadorX == 50){
+            if (temp != 0 && contadorO == 50 && contadorX == 50) {
                 temp = 0;
-            }else if(temp != 0 && temp != 1 && contadorX == 50){
-                while(temp == 2){
-                    //printf("temporal 2 (X) %d\n",temp);
+            } else if (temp != 0 && temp != 1 && contadorX == 50) {
+                while (temp == 2) {
                     temp = rand() % 3;
-
                 }
-            }else if(temp != 0 && temp != 2 && contadorO == 50){
-                while(temp == 1){
-                    //printf("temporal 1 (O) %d\n",temp);
+            } else if (temp != 0 && temp != 2 && contadorO == 50) {
+                while (temp == 1) {
                     temp = rand() % 3;
                 }
             }
 
-            if(temp == 1){
+            if (temp == 1) {
                 *(tablero + i * row + j) = 'O';
-                contadorO ++;
-            }else if(temp == 2){
+                contadorO++;
+            } else if (temp == 2) {
                 *(tablero + i * row + j) = 'X';
-                contadorX ++;
-            }else{
+                contadorX++;
+            } else {
                 *(tablero + i * row + j) = ' ';
             }
         }
     }
-    printf("contador O = %i\ncontador X = %i\n",contadorO,contadorX);
+    printf("contador O = %i\ncontador X = %i\n", contadorO, contadorX);
 }
 
-void imprimirTablero(char *tablero,int col,int row){
-
-    for (int i = 0; i < 25; ++i) {
+void imprimirTablero(char *tablero, int col, int row) {
+    for (int i = 0; i < col; ++i) {
         printf("\n");
-        for (int j = 0; j < 25; ++j) {
-            printf(" %c ",*(tablero + i * row + j));
+        for (int j = 0; j < row; ++j) {
+            printf(" %c ", *(tablero + i * row + j));
+        }
+    }
+}
+
+int contarVecinos(char *tablero, int col, int row, int x, int y) {
+    int contador = 0;
+
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            if (i == 0 && j == 0) {
+                continue;  // No contar la celda actual
+            }
+
+            int nuevaX = x + i;
+            int nuevaY = y + j;
+
+            if (nuevaX >= 0 && nuevaX < col && nuevaY >= 0 && nuevaY < row &&
+                *(tablero + nuevaX * row + nuevaY) != ' ') {
+                contador++;
+            }
         }
     }
 
+    return contador;
+}
+
+void GuerraPorLaVida(char *tablero, int col, int row,char * tableroTemporal) {
+
+
+    for (int i = 0; i < col; ++i) {
+        for (int j = 0; j < row; ++j) {
+            int vecinos = contarVecinos((char *)tablero, col, row, i, j);
+            char estadoActual = *(tablero + i * row + j);
+
+            if (estadoActual == ' ') {
+
+                if (vecinos == 3) {
+                    if (contarVecinos((char *)tablero, col, row, i, j) >= 2) {
+                        *(tableroTemporal + i * row + j) = 'X';
+                    } else {
+                        *(tableroTemporal + i * row + j) = 'O';
+                    }
+                } else {
+                    *(tableroTemporal + i * row + j) = ' ';
+                }
+            } else {
+
+                if (vecinos < 2 || vecinos > 3 || contarVecinos((char *)tablero, col, row, i, j) < 2) {
+                    *(tableroTemporal + i * row + j) = ' ';
+                } else {
+                    *(tableroTemporal + i * row + j) = estadoActual;
+                }
+            }
+        }
+    }
+
+
+    for (int i = 0; i < col; ++i) {
+        for (int j = 0; j < row; ++j) {
+            *(tablero + i * row + j) = *(tableroTemporal + i * row + j);
+        }
+    }
 }
 
 int main() {
-
     int flag = 0;
     char tablero[25][25];
+    char tableroTemporal[25][25];
 
-    crearTablero((char *)tablero,25,25);
+    crearTablero((char *)tablero, 25, 25);
 
-    while (flag != 1){
-
-        imprimirTablero((char *)tablero,25,25);
-
+    while (flag != 1) {
+        imprimirTablero((char *)tablero, 25, 25);
 
         printf("\n");
-
         printf("Para continuar presiona 2\nSi quieres terminar presiona 1\n");
-        scanf("%i",&flag);
+        scanf("%i", &flag);
 
-
-        //aqui se evalúa el juego de la vida
-
+        // Aquí se evalúa el juego de la vida
+        GuerraPorLaVida((char *)tablero, 25, 25,(char *)tableroTemporal);
     }
-
 
     return 0;
 }
